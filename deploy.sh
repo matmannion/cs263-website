fd89f6b9faf2f48219030b4e593cf630d3417836
Michael Gale <m.gale@warwick.ac.uk>
2020-10-15 17:39:57 +0100

    Add instructions for online labs

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ SB_ROOT=/fac/sci/dcs/teaching/material/cs263
 chmod +x $SB_UTIL
 
 $SB_UTIL edit --page=$SB_ROOT --file=material.html
+$SB_UTIL edit --page=$SB_ROOT/online-labs --file=online-labs.html
